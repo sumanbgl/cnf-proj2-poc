bbbe79b932350cee909eaddc30e5bba05ae856ff
sumanbgl <suman.bgl@gmail.com>
2021-12-23 12:49:04 +0530

    Create kafka_producer.py

---
+++ kafka_producer.py
@@ -0,0 +1,13 @@
+from kafka import KakfaProducer
+
+TOPIC_NAME = 'orders'
+KAFKA_SERVER = 'localhost:9092'
+
+producer = KakfaProducer(bootstrap_servers=KAFKA_SERVER)
+
+order_info = '{"person_id":"5", "latitude":"90", "longitude":"100"}'
+
+producer.send('orders',order_info)
+producer.flush()
+
+
