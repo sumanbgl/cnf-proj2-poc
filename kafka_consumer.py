ccb2cb2e10bd4de45dc4af0600f3766fcffc9370
sumanbgl <suman.bgl@gmail.com>
2021-12-23 13:21:29 +0530

    Update kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,9 +1,9 @@
-from kafka import KafaConsumer
+from kafka import KafkaConsumer
 import json
 
 TOPIC_NAME = 'orders'
 
-consumer = KakfaConsumer(TOPIC_NAME,
+consumer = KafkaConsumer(TOPIC_NAME,
                         value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 for message in consumer:
