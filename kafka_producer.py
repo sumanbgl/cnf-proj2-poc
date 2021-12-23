21840f8335eee3db8e3066513d9f965281a45e45
sumanbgl <suman.bgl@gmail.com>
2021-12-23 13:18:35 +0530

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -4,11 +4,12 @@ import json
 TOPIC_NAME = 'orders'
 KAFKA_SERVER = 'localhost:9092'
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+producer = KakfaProducer(bootstrap_servers=KAFKA_SERVER, 
+                        value_serializer=lambda v : dumps(v).encode('utf-8'))
 
 order_info = {"person_id":"5", "latitude":"90", "longitude":"100"}
 
-producer.send('orders',json.dumps(order_info).encode('utf-8'))
+producer.send('orders', value=order_info)
 producer.flush()
 
 
