1e8de34b9e37b1d6e211ee695985442cb3b4aea0
sumanbgl <suman.bgl@gmail.com>
2021-12-23 13:10:37 +0530

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -8,7 +8,7 @@ producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
 order_info = {"person_id":"5", "latitude":"90", "longitude":"100"}
 
-producer.send('orders',json.dump(order_info).encode('utf-8'))
+producer.send('orders',json.dumps(order_info).encode('utf-8'))
 producer.flush()
 
 
